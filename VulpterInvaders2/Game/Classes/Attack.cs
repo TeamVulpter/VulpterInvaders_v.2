4e6d22b6a5cc826815d8ad49adda4abf2799c339
kkoceva <k_koceva@yahoo.com>
2015-06-16 12:20:57 +0300

    Some classes and interfaces added

---
+++ VulpterInvaders2/Game/Classes/Attack.cs
@@ -0,0 +1,17 @@
+﻿using Game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Classes
+{
+    public class Attack :IAttack
+    {
+        public void UpdateAttack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
