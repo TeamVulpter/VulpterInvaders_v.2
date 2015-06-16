799405e4a60d573dfb0f57d38d6a7fec5771da5d
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-16 07:33:00 +0300

    Add Player Class and inheritance Hero class

---
+++ VulpterInvaders2/Game/Classes/Hero/Player.cs
@@ -0,0 +1,45 @@
+﻿namespace Game.Classes.Hero
+{
+    class Player : Hero
+    {
+        public Player(string name)
+        {
+            
+        }
+
+        public override void AddHealth()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void RemoveHealth()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void AddLives()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void RemoveLives()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void AddScores()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void RemoveScores()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Mooving(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
