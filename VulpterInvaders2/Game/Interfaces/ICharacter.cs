9e270eadfb4730ec65f1b3b212921cf38b64df31
kkoceva <k_koceva@yahoo.com>
2015-06-18 02:08:35 +0300

    ICharacter repaired

---
+++ VulpterInvaders2/Game/Interfaces/ICharacter.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.Interfaces
+{
+    interface ICharacter
+    {
+        void CreateHero(int positionX, int positionY, string heroName);
+        void KillHero();
+
+        void AddHealth(int points);
+        void RemoveHealth(int points);
+    }
+}
