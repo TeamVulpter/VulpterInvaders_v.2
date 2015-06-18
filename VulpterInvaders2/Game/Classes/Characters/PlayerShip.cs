c93702c56fbaaea53403038164e0eb55d8eb3ab5
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-18 21:58:48 +0300

    implement in PlayerShip methods: AddHealth, AddLives, AddScores, RemoveHealth, RemoveLives, RemoveScores;

--- VulpterInvaders2/Game/Classes/Characters/PlayerShip.cs
+++ VulpterInvaders2/Game/Classes/Characters/PlayerShip.cs
@@ -7,36 +7,66 @@ namespace Game.Classes.Characters
         public PlayerShip(int positionX, int positionY)
             : base(positionX, positionY)
         {
-
         }
+
         public override void AddHealth(int points)
         {
-            throw new NotImplementedException();
+            if (points < 0)
+            {
+                //TODO add exception or message;
+                return;
+            }
+            this.Health += points;
         }
 
         public override void RemoveHealth(int points)
         {
-            throw new NotImplementedException();
+            if (points < 0)
+            {
+                //TODO add exception or message;
+                return;
+            }
+            this.Health -= points;
         }
 
         public override void AddLives(int life)
         {
-            throw new NotImplementedException();
+            if (life < 0)
+            {
+                //TODO add exception or message;
+                return;
+            }
+            this.Lives += life;
         }
 
         public override void RemoveLives(int life)
         {
-            throw new NotImplementedException();
+            if (life < 0)
+            {
+                //TODO add exception or message;
+                return;
+            }
+            this.Lives -= life;
         }
 
         public override void AddScores(int score)
         {
-            throw new NotImplementedException();
+            if (score < 0)
+            {
+                //TODO add exception or message;
+                return;
+            }
+            this.Scores += score;
         }
 
         public override void RemoveScores(int score)
         {
-            throw new NotImplementedException();
+            if (score < 0)
+            {
+                //TODO add exception or message;
+                return;
+            }
+            this.Scores -= score;
         }
 
         public override void Mooving(int x, int y)
