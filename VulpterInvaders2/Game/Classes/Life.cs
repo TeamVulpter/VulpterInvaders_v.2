0d10c583e8d7203aefe3290c2f4e7d58a50e4525
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-18 13:47:16 +0300

    revert Health, Life and Score

---
+++ VulpterInvaders2/Game/Classes/Life.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Classes
+{
+    public class Life
+    {
+        static Life()
+        {
+            LifeCount = 3;
+        }
+
+        public static int LifeCount { get; set; }
+    }
+}
---
+++ VulpterInvaders2/Game/Classes/Score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Classes
+{
+    public class Score
+    {
+        static Score()
+        {
+            ScoreCount = 0;
+        }
+
+        public static int ScoreCount { get; set; }
+    }
+}
