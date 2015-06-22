44003bf478ed6b6eb228ecca64ca6b8caa410b56
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-22 23:21:47 +0300

    remove from FormMenuUI generate player; add comment in VulpterInvadersGame; remove row with field Bonus bonus;

--- VulpterInvaders2/Game/FormMenuUI.cs
+++ VulpterInvaders2/Game/FormMenuUI.cs
@@ -16,18 +16,9 @@ namespace Game
 
         private void Btn_StartNewGame_Click(object sender, EventArgs e)
         {
-            //if (this.player != null)
-            //{
-                
-                VulpterInvadersGame newGame = new VulpterInvadersGame();
-                this.Hide();
-                newGame.Show();
-                
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Not created hero. First create hero and then try again!");
-            //}
+            VulpterInvadersGame newGame = new VulpterInvadersGame();
+            this.Hide();
+            newGame.Show();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -42,21 +33,6 @@ namespace Game
 
         //private void Btn_NewPlayer_Click(object sender, EventArgs e)
         //{
-        //    try
-        //    {
-        //        //create hero
-        //        this.player = new Player(350, 560, hero);
-        //        //add default values
-        //        this.player.AddHealth(Health.HelthCount);
-        //        this.player.AddLives(Life.LifeCount);
-        //        this.player.AddScores(Score.ScoreCount);
-        //        MessageBox.Show("Hero is created!");
-        //    }
-        //    catch (NotImplementedException ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-            
         //}
     }
 }
