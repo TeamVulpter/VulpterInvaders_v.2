8599595980542240aa888f961b1e43858ec28b29
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-22 23:25:25 +0300

    Remove from Character fields:     private string name;     private int health;     private int lives;     private int scores;

--- VulpterInvaders2/Game/Classes/Characters/Character.cs
+++ VulpterInvaders2/Game/Classes/Characters/Character.cs
@@ -3,11 +3,6 @@
     using Interfaces;
     public abstract class Character : GameObject, ICharacter
     {
-        private string name;
-        private int health;
-        private int lives;
-        private int scores;
-
         protected Character(int positionX, int positionY)
             : base(positionX, positionY)
         {
