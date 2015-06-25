d8c4fca77f95aaebc3a4e04daaba2e5b0bea0fb5
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-25 14:01:50 +0300

    Change IMap Interface; Change Map without matrix to know the details of the form to be used for bonuses and character in motion;

--- VulpterInvaders2/Game/Classes/Matrix/Map.cs
+++ VulpterInvaders2/Game/Classes/Matrix/Map.cs
@@ -1,106 +1,52 @@
-﻿using System.Windows.Forms;
-
-namespace Game.Classes.Matrix
+﻿namespace Game.Classes.Matrix
 {
-    using System;
     using Interfaces;
     public class Map : IMap
     {
-        //private Brick brick;
-        //public Map(int[,] matrix)
-        //{
-        //    this.Matrix = new int[40,40];
-        //}
-        //public int[,] Matrix { get; private set; }
+        private int top;
+        private int down;
+        private int left;
+        private int right;
 
-        public int[,] DrawField()
+        public Map(int top, int down, int left, int right)
         {
-            int[,] matrix = new int[40, 400];
-            Random randomWidthGenerator = new Random();
-            Random randomValue = new Random();
-            Random randomDigit = new Random();
-            Random randomLifeChance = new Random();
-            int chance = randomLifeChance.Next(1, 100);
+            this.Top = top;
+            this.Down = down;
+            this.Left = left;
+            this.Right = right;
+        }
 
-            //exit
-            matrix[1, 1] = 8;
+        public int Top
+        {
+            get { return this.top; }
+            set { this.top = value; }
+        }
+        public int Down
+        {
+            get { return this.down; }
+            set { this.down = value; }
+        }
+        public int Left
+        {
+            get { return this.left; }
+            set { this.left = value; }
+        }
+        public int Right
+        {
+            get { return this.right; }
+            set { this.right = value; }
+        }
 
-            int height = matrix.GetLength(0);
-            int width = matrix.GetLength(1);
-
-            //draw matrix borders
-            for (int brick = 0; brick < height; brick++)
+        public int[] MapLacation()
+        {
+            int[] location = new []
             {
-                matrix[brick, 0] = 1;
-                matrix[0, brick] = 1;
-                matrix[height - 1, brick] = 1;
-                matrix[brick, height - 1] = 1;
-            }
-            
-            //draw matrix grid
-            for (int row = 5; row < height - 5; row += 5)
-            {
-                for (int col = 0; col < width; col++)
-                {
-                    matrix[row, col] = 1;
-                }
-            }
-            
-            //draw doors
-            for (int row = 1; row < height - 1; row++)
-            {
-                int randomHole = randomValue.Next(1, width - 1);
-                if (matrix[row, randomHole] == 1)
-                {
-                    if (matrix[row, randomHole] != 8)
-                    {
-                        matrix[row, randomHole] = 0;
-                    }
-                }
-            }
-
-            //draw digits
-            for (int row = 1; row < height - 1; row++)
-            {
-                int randomCol = randomWidthGenerator.Next(1, width - 1);
-                int random = randomDigit.Next(3, 9);
-                if (matrix[row, randomCol] == 0)
-                {
-                    matrix[row, randomCol] = random;
-                }
-            }
-
-            //for (int row = 0; row < height; row++)
-            //{
-            //    for (int col = 0; col < width; col++)
-            //    {
-            //        if (matrix[row, col] == 1)
-            //        {
-            //            brick.PositionX = row;
-            //            brick.PositionY = col;
-            //            //Visualization.PrintCharMatrixAtPosition(row, col, '#', ConsoleColor.White);
-            //        }
-
-            //        else if (matrix[row, col] >= 3 && matrix[row, col] <= 6)
-            //        {
-            //            //Visualization.PrintCharMatrixAtPosition(row, col, (char)(matrix[row, col] + 48), ConsoleColor.Blue);
-            //        }
-            //        else if (matrix[row, col] == 7 && chance > 20)
-            //        {
-            //            //Visualization.PrintCharMatrixAtPosition(row, col, '\u2665', ConsoleColor.Red);
-            //        }
-            //        else if (matrix[row, col] == 8)
-            //        {
-            //            //Visualization.PrintCharMatrixAtPosition(row, col, '\u0065', ConsoleColor.Green);
-            //        }
-            //        else
-            //        {
-            //            //Console.Write(" ");
-            //        }
-            //    }
-            //    //Console.WriteLine();
-            //}
-            return matrix;
+                this.Top,
+                this.Left,
+                this.Down,
+                this.Right
+            };
+            return location;
         }
     }
 }
--- VulpterInvaders2/Game/Interfaces/IMap.cs
+++ VulpterInvaders2/Game/Interfaces/IMap.cs
@@ -2,6 +2,6 @@
 {
     public interface IMap
     {
-        int[,] DrawField();
+        int[] MapLacation();
     }
 }
