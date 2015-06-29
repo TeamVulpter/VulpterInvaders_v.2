67e54eff45d036cdc01c7864d1b2f4ed7b8b8acc
kkoceva <k_koceva@yahoo.com>
2015-06-29 11:22:53 +0300

    Game update

--- VulpterInvaders2/Game/Classes/Factory/EnemyInvaderFactory.cs
+++ VulpterInvaders2/Game/Classes/Factory/EnemyInvaderFactory.cs
@@ -1,6 +1,12 @@
-﻿namespace Game.Classes.Factory
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Classes.Factory
 {
     public class EnemyInvaderFactory
     {
+        private Random randomInvader = new Random();
+        private List<int> rndX = new List<int>();
+        private List<int> rndY = new List<int>(); 
     }
 }
--- VulpterInvaders2/Game/InvadersAttack.Designer.cs
+++ VulpterInvaders2/Game/InvadersAttack.Designer.cs
@@ -34,6 +34,13 @@
             this.shipEnemy = new System.Windows.Forms.Panel();
             this.bulletPanel = new System.Windows.Forms.Panel();
             this.playerShip = new System.Windows.Forms.PictureBox();
+            this.invaderTwo = new System.Windows.Forms.Panel();
+            this.invaderThree = new System.Windows.Forms.Panel();
+            this.invaderFour = new System.Windows.Forms.Panel();
+            this.invaderFive = new System.Windows.Forms.Panel();
+            this.invaderSix = new System.Windows.Forms.Panel();
+            this.invaderSeven = new System.Windows.Forms.Panel();
+            this.invaderEight = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.playerShip)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,11 +77,74 @@
             this.playerShip.TabIndex = 0;
             this.playerShip.TabStop = false;
             // 
+            // invaderTwo
+            // 
+            this.invaderTwo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderTwo.BackgroundImage")));
+            this.invaderTwo.Location = new System.Drawing.Point(410, 66);
+            this.invaderTwo.Name = "invaderTwo";
+            this.invaderTwo.Size = new System.Drawing.Size(43, 36);
+            this.invaderTwo.TabIndex = 3;
+            // 
+            // invaderThree
+            // 
+            this.invaderThree.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderThree.BackgroundImage")));
+            this.invaderThree.Location = new System.Drawing.Point(490, 66);
+            this.invaderThree.Name = "invaderThree";
+            this.invaderThree.Size = new System.Drawing.Size(43, 36);
+            this.invaderThree.TabIndex = 4;
+            // 
+            // invaderFour
+            // 
+            this.invaderFour.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderFour.BackgroundImage")));
+            this.invaderFour.Location = new System.Drawing.Point(264, 66);
+            this.invaderFour.Name = "invaderFour";
+            this.invaderFour.Size = new System.Drawing.Size(43, 36);
+            this.invaderFour.TabIndex = 5;
+            // 
+            // invaderFive
+            // 
+            this.invaderFive.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderFive.BackgroundImage")));
+            this.invaderFive.Location = new System.Drawing.Point(490, 136);
+            this.invaderFive.Name = "invaderFive";
+            this.invaderFive.Size = new System.Drawing.Size(43, 36);
+            this.invaderFive.TabIndex = 6;
+            // 
+            // invaderSix
+            // 
+            this.invaderSix.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderSix.BackgroundImage")));
+            this.invaderSix.Location = new System.Drawing.Point(410, 136);
+            this.invaderSix.Name = "invaderSix";
+            this.invaderSix.Size = new System.Drawing.Size(43, 36);
+            this.invaderSix.TabIndex = 7;
+            // 
+            // invaderSeven
+            // 
+            this.invaderSeven.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderSeven.BackgroundImage")));
+            this.invaderSeven.Location = new System.Drawing.Point(331, 136);
+            this.invaderSeven.Name = "invaderSeven";
+            this.invaderSeven.Size = new System.Drawing.Size(43, 36);
+            this.invaderSeven.TabIndex = 8;
+            // 
+            // invaderEight
+            // 
+            this.invaderEight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("invaderEight.BackgroundImage")));
+            this.invaderEight.Location = new System.Drawing.Point(264, 136);
+            this.invaderEight.Name = "invaderEight";
+            this.invaderEight.Size = new System.Drawing.Size(43, 36);
+            this.invaderEight.TabIndex = 9;
+            // 
             // InvadersAttack
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 563);
+            this.Controls.Add(this.invaderEight);
+            this.Controls.Add(this.invaderSeven);
+            this.Controls.Add(this.invaderSix);
+            this.Controls.Add(this.invaderFive);
+            this.Controls.Add(this.invaderFour);
+            this.Controls.Add(this.invaderThree);
+            this.Controls.Add(this.invaderTwo);
             this.Controls.Add(this.shipEnemy);
             this.Controls.Add(this.bulletPanel);
             this.Controls.Add(this.playerShip);
@@ -94,5 +164,12 @@
         private System.Windows.Forms.Panel bulletPanel;
         private System.Windows.Forms.Panel shipEnemy;
         private System.Windows.Forms.Timer timerMovement;
+        private System.Windows.Forms.Panel invaderTwo;
+        private System.Windows.Forms.Panel invaderThree;
+        private System.Windows.Forms.Panel invaderFour;
+        private System.Windows.Forms.Panel invaderFive;
+        private System.Windows.Forms.Panel invaderSix;
+        private System.Windows.Forms.Panel invaderSeven;
+        private System.Windows.Forms.Panel invaderEight;
     }
 }
