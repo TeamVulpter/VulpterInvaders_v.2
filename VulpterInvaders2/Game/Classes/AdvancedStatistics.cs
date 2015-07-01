b8355495d6e2ff39f02963d5b19e3cdeb8f4f223
kkoceva <k_koceva@yahoo.com>
2015-07-01 11:31:22 +0300

    Class added

---
+++ VulpterInvaders2/Game/Classes/AdvancedStatistics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game.Classes
+{
+    public class AdvancedStatistics
+    {
+        public static void GameExit()
+        {//if lives is zero or negative - game over
+            if (Life.LifeCount <= 0)
+            {
+                MessageBox.Show("Game over");
+                Application.Exit();
+            }
+        }
+
+        public static void GameLifeReduction()
+        {
+            if (Health.HelthCount <= 0)
+            {
+                Life.LifeCount -= 1;
+            }
+        }
+
+        public static void GameScoreMax()
+        {
+            if (Score.ScoreCount >= 100)
+            {
+                MessageBox.Show("Game over");
+                Application.Exit();
+            }
+        }
+
+        public static void GameHealthMax()
+        {
+            if (Health.HelthCount >= 250)
+            {
+                Life.LifeCount += 1;
+                Health.HelthCount = 150;
+            }
+        }
+    }
+}
