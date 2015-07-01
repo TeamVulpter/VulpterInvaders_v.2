855bd4e8bf5f01de4151ef936691cf51b9ec0398
kkoceva <k_koceva@yahoo.com>
2015-07-02 00:32:16 +0300

    Collision enemy bullet player

--- VulpterInvaders2/Game/InvadersAttack.cs
+++ VulpterInvaders2/Game/InvadersAttack.cs
@@ -108,17 +108,16 @@ namespace Game
             this.enemyShot = new BulletEnemy(bulletEnemy);
             enemyShot.EnemyBullet.Location = new Point(enemyShot.PositionX, enemyShot.PositionY + 10);
 
-            if (enemyShot.PositionY >= 470)
+            if (enemyShot.PositionY >= 500)
             {
                 enemyShot.EnemyBullet.Location = new Point(enemies[randomEnemy.Next(1, 6)].PositionX, enemies[randomEnemy.Next(1, 6)].PositionY + 10);
             }
             enemyShot.Start();
-            if (playerShip.Location.X >= enemyShot.EnemyBullet.Location.X && playerShip.Location.X <= (enemyShot.EnemyBullet.Location.X + enemyShot.EnemyBullet.Width) && playerShip.Location.Y >= enemyShot.EnemyBullet.Location.Y &&
-                        (playerShip.Location.Y) <= enemyShot.EnemyBullet.Location.Y)
+            if (playerShip.Location.X <= enemyShot.EnemyBullet.Location.X && playerShip.Location.X+playerShip.Width >= (enemyShot.EnemyBullet.Location.X) &&
+                        playerShip.Location.Y+20 <= enemyShot.EnemyBullet.Location.Y)
             {
                 Life.LifeCount -= 1;
                 this.life_value.Text = Life.LifeCount.ToString();
-
             }
             foreach (var enemy in enemies)
             {
