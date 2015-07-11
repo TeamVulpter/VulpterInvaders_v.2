f4a8984684c3063f9b92caaa3b2815816f090a42
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 22:44:10 +0300

    add map field and add Map property

--- VulpterInvaders2/Game/Loader/GameLoader.cs
+++ VulpterInvaders2/Game/Loader/GameLoader.cs
@@ -2,15 +2,31 @@ namespace Game.Loader
 {
     using System.Windows.Forms;
 
+    using Game.Interfaces;
+
     public class GameLoader
     {
+        private IMap map;
 
         public void Run()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMenuUI());
+            Application.Run(new FormMenuUI(this));
             Application.Exit();
+        }
+
+        public IMap Map
+        {
+            get
+            {
+                return this.map;
+            }
+
+            set
+            {
+                this.map = value;
+            }
         }
 
         private void LoadItems()
