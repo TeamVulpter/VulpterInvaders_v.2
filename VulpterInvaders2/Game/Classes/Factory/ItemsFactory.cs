6f11c8589d9bcc95b607288911ef513095a5f7ed
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 22:36:57 +0300

    refactoring ItemsFactory

--- VulpterInvaders2/Game/Classes/Factory/ItemsFactory.cs
+++ VulpterInvaders2/Game/Classes/Factory/ItemsFactory.cs
@@ -1,9 +1,8 @@
-﻿using System.Linq;
-
-namespace Game.Classes.Factory
+﻿namespace Game.Classes.Factory
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Windows.Forms;
 
     using Interfaces;
@@ -12,9 +11,9 @@ namespace Game.Classes.Factory
 
     public class ItemsFactory
     {
-        private Random rnd = new Random();
-        private List<int> rndX = new List<int>();
-        private List<int> rndY = new List<int>();
+        private readonly Random rnd = new Random();
+        private readonly List<int> rndX = new List<int>();
+        private readonly List<int> rndY = new List<int>();
         private IMap map;
         private int maxRandomX;
         private int maxRandomY;
@@ -29,41 +28,38 @@ namespace Game.Classes.Factory
                 int randomItem = rnd.Next(1, 5);
                 this.maxRandomX = this.map.Right - pic[i].Width;
                 this.maxRandomY = this.map.Down - pic[i].Height;
-                ItemType type = (ItemType) Enum.Parse(typeof (ItemType), randomItem.ToString());
+                ItemType type = (ItemType)Enum.Parse(typeof(ItemType), randomItem.ToString());
 
                 items.Add(new Item(
                                     this.GenerateRandomX(this.map.Left, this.maxRandomX),
-                                    this.GenerateRandomY(this.map.Top, this.maxRandomY), 
-                                    type, 
-                                    pic[i]
-                                   )
-                         );
+                                    this.GenerateRandomY(this.map.Top, this.maxRandomY),
+                                    type,
+                                    pic[i]));
             }
+
             return items;
         }
 
-        //create random x position
         private int GenerateRandomX(int minX, int maxX)
         {
             int randomX = rnd.Next(minX, maxX);
-            var randX = rndX.Any(x => x == randomX);
-            if (randX)
+            if (this.rndX.Any(x => x == randomX))
             {
                 return this.GenerateRandomX(this.map.Left, this.maxRandomX);
             }
+
             this.rndX.Add(randomX);
             return randomX;
         }
 
-        //create random y position
         private int GenerateRandomY(int minY, int maxY)
         {
             int randomY = rnd.Next(minY, maxY);
-            var randY = rndY.Any(y => y == randomY);
-            if (randY)
+            if (this.rndY.Any(y => y == randomY))
             {
                 return this.GenerateRandomY(this.map.Top, this.maxRandomY);
             }
+
             this.rndY.Add(randomY);
             return randomY;
         }
