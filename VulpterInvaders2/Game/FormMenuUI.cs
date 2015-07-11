b599dafd071f88cb2217aa30fe297ae4a86909ea
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 22:55:21 +0300

    update first form

--- VulpterInvaders2/Game/FormMenuUI.cs
+++ VulpterInvaders2/Game/FormMenuUI.cs
@@ -4,20 +4,25 @@
     using System.Media;
     using System.Windows.Forms;
 
+    using Game.Loader;
+
     public partial class FormMenuUI : Form
     {
-        private SoundPlayer musicPlayer;
-        public FormMenuUI()
+        private readonly SoundPlayer musicPlayer;
+
+        private GameLoader engineGameLoader;
+
+        public FormMenuUI(GameLoader engineGameLoader)
         {
-            InitializeComponent();
-            musicPlayer = new System.Media.SoundPlayer("../../Resources/Song/GameMusic.wav");
-            //start music player
-            musicPlayer.PlayLooping();
+            this.engineGameLoader = engineGameLoader;
+            this.InitializeComponent();
+            this.musicPlayer = new System.Media.SoundPlayer("../../Resources/Song/GameMusic.wav");
+            this.musicPlayer.PlayLooping();
         }
 
         private void Btn_StartNewGame_Click(object sender, EventArgs e)
         {
-                VulpterInvadersGame newGame = new VulpterInvadersGame();
+                VulpterInvadersGame newGame = new VulpterInvadersGame(this.engineGameLoader);
                 newGame.Show();
                 this.Hide();
         }
